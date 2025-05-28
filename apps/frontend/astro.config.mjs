a98caa8dbc7c41da09e170e06361fb3ac019c99a
Jorge Tristan <Jorge-tristan@Josues-MacBook-Air.local>
2025-05-28 11:59:37 -0600

    fix: change to static output for railway compatibility

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,16 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   srcDir: './src',
   outDir: './dist',
   vite: {
