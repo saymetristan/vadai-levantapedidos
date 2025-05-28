92a8c7a238fbb3d9f7237944f05ad7227fc6d82d
Jorge Tristan <Jorge-tristan@Josues-MacBook-Air.local>
2025-05-28 11:49:40 -0600

    add astro node adapter for railway deployment

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  }),
   srcDir: './src',
   outDir: './dist',
   vite: {
